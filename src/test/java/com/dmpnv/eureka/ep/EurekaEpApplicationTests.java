dea2747d90271a084f692980d3471c4116f4090f
dp <dp@localhost>
2020-09-17 22:20:59 +0200

    project initial state

---
+++ src/test/java/com/dmpnv/eureka/ep/EurekaEpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmpnv.eureka.ep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaEpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
